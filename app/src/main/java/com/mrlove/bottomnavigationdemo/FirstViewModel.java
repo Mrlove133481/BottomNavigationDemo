2595c0aaff8071c44e056e29e02df460767a93e1
Mrlove133481 <1334819077@qq.com>
2020-02-11 16:14:23 +0800

    初始化提交

---
+++ app/src/main/java/com/mrlove/bottomnavigationdemo/FirstViewModel.java
@@ -0,0 +1,7 @@
+package com.mrlove.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class FirstViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/mrlove/bottomnavigationdemo/ThirdViewModel.java
@@ -0,0 +1,7 @@
+package com.mrlove.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class ThirdViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
